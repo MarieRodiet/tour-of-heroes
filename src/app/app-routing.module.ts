b86c1efe2c5a2a6e9fd2558e1ff926de95f92171
MarieRodiet <marie.rodiet@gmail.com>
2023-01-06 12:18:15 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HeroesComponent } from "./heroes/heroes.component";
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-const routes: Routes = [];
+// Routes tell the Router which view to display for which URL
+const routes: Routes = [
+{path: '', redirectTo: '/dashboard', pathMatch:'full'},
+{path:'heroes', component: HeroesComponent},
+{path: 'dashboard', component: DashboardComponent},
+{path: 'detail/:id', component: HeroDetailComponent}];
 
+//The @NgModule metadata initializes the router and starts it listening for browser location changes.
 @NgModule({
+//The method is called forRoot() because you configure the router at the application's root level.
   imports: [RouterModule.forRoot(routes)],
+//AppRoutingModule exports RouterModule to be available throughout the application
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
